8d53106bbe70bbbb813c3eeef65d11828ae99d7f
Grivn Wang <grivn.wang@gmail.com>
2021-10-19 21:47:09 +0800

    feat(*): upgrade comments

--- metapool/tracker/tracker.go
+++ metapool/tracker/tracker.go
@@ -5,11 +5,13 @@ import (
 	"github.com/Grivn/phalanx/common/types"
 )
 
+// CommandTracker is used to record received commands.
 type CommandTracker interface {
 	RecordCommand(command *protos.Command)
 	ReadCommand(digest string) *protos.Command
 }
 
+// PartialTracker is used to record received partial orders.
 type PartialTracker interface {
 	RecordPartial(pOrder *protos.PartialOrder)
 	ReadPartial(idx types.QueryIndex) *protos.PartialOrder
